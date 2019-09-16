db5958730f77cb9ad7d4d1cc3e3fcecb75f00250
tezizzm <martezkillens@gmail.com>
2019-09-16 12:12:45 -0700

    slight modifications to fix dyanamic logging

--- DynamicLoggingPOC/DynamicLoggingPOC/Startup.cs
+++ DynamicLoggingPOC/DynamicLoggingPOC/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.Management.CloudFoundry;
 
 namespace DynamicLoggingPOC
 {
@@ -26,6 +27,8 @@ namespace DynamicLoggingPOC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // CODE ADDED BELOW
+            services.AddCloudFoundryActuators(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +44,8 @@ namespace DynamicLoggingPOC
                 app.UseHsts();
             }
 
+            // CODE ADDED BELOW
+            app.UseCloudFoundryActuators();
             app.UseHttpsRedirection();
             app.UseMvc();
         }

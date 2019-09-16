8333a414ca3111fbe2f5ead0c53aa631b0e6bfe5
Brock Pond <pondy311@gmail.com>
2019-09-16 11:09:12 -0600

    dynamic logging POC

---
+++ DynamicLoggingPOC/DynamicLoggingPOC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
+
+namespace DynamicLoggingPOC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    config.SetBasePath(builderContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddCloudFoundry();
+                })
+                .ConfigureLogging((builderContext, loggingBuilder) =>
+                    {
+                        loggingBuilder.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+                        loggingBuilder.AddDynamicConsole();
+                    })
+                .UseStartup<Startup>();
+    }
+}
